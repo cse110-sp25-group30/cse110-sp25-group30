5e29e9838051b1f816c56cd90ce91767e26905e2
jcohenwang <jcohenwang@ucsd.edu>
2025-05-22 16:02:37 -0700

    add some documentation to frog-card component

--- components/frog-card.js
+++ components/frog-card.js
@@ -1,11 +1,7 @@
-
 /*
 This is the JavaScript file for the frog-card component.
-We need to add a flipping animation to the card so it can be flipped to show the back
-Bio and course should go on the back of the card. 
-
-Try to use CSS variables for the colors. 
 */
+
 class FrogCard extends HTMLElement {
 	constructor() {
 		super();
@@ -43,8 +39,6 @@ class FrogCard extends HTMLElement {
 			flex-direction: column;
 			align-items: center;
 			justify-content: flex-start;
-			background: transparent;
-			border: none;
 		}
 
 		.flip-card-back {
@@ -53,8 +47,8 @@ class FrogCard extends HTMLElement {
 
 		.face {
 			width: auto;
-			height: 53%;
-			max-width: 71%;
+			height: 53%; /* sets height to fit the height of the frame */
+			max-width: 71%; /* makes sure if the image is landscape, it does not go past the border of the card */
 			margin-top: 30%;
 			position: relative;
 			z-index: 1;
@@ -130,10 +124,11 @@ class FrogCard extends HTMLElement {
 
 		const card = this.shadowRoot.querySelector('card');
 
+		/* changes the color of the text from ucsd yellow to blue if the card is of legendary rarity */
+		this.style.setProperty('--text-color', '#f2a900'); // ucsd yellow
+		
 		if (data.rarity === "legendary") {
 			this.style.setProperty('--text-color', '#003057'); // ucsd blue
-		} else {
-			this.style.setProperty('--text-color', '#f2a900'); // ucsd yellow
 		}
 
 		card.innerHTML = `
@@ -141,16 +136,23 @@ class FrogCard extends HTMLElement {
 			<div class="flip-card-inner">
 				<!-- FRONT -->
 				<div class="flip-card-front">
+					<!-- PROF IMAGE -->
 					<img class="face" src="${data.imgSrc}" alt="${data.name} image">
+					<!-- CARD OVERLAY -->
 					<img class="card-fg" src="./assests/${data.rarity}_front.png" alt="foreground layer">
+					<!-- PROF NAME -->
 					<p class="front-name">${data.name}</p>
 				</div>
 
 				<!-- BACK -->
 				<div class="flip-card-back">
+					<!-- CARD BACKGROUND -->
 					<img class="back-bg" src="./assests/${data.rarity}_back.png" alt="background">
+					<!-- PROF NAME -->
 					<p class="back-name">${data.name}</p>
+					<!-- PROF BIO -->
 					<p class="bio">${data.bio}</p>
+					<!-- COURSE -->
 					<p class="course">${data.course}</p>
 				</div>
 			</div>
