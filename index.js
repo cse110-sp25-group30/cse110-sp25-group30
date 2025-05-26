56421b27445fc42ee1ac6e801236e6f8cbe3dda9
KevinISun <114264552+KevinISun@users.noreply.github.com>
2025-05-25 20:36:41 -0700

    MVP: Clicker Game

--- index.js
+++ index.js
@@ -198,11 +198,19 @@ async function init() {
   createCard(card_data[selected_card]);
   card_button_click();
 
-  //redirect to grid.html when view all button is clicked
+  // redirect to grid.html when view all button is clicked
   const viewAllBtn = document.getElementById("view-all");
   if (viewAllBtn) {
     viewAllBtn.addEventListener("click", () => {
       window.location.href = "grid.html";
     });
+  }
+
+  // redirect to clicker.html when 
+  const viewClickerButton = document.getElementById("view-clicker");
+  if ("view-clicker") {
+    viewClickerButton.addEventListener("click", () => {
+      window.location.href = "clicker.html";
+    })
   }
 }
--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,5 +1,6 @@
 /* eslint-disable-next-line no-unused-vars */
 import {update_points} from "../index.js"
+import {fetch_user_info} from "../index.js"
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -11,24 +12,56 @@ window.addEventListener("DOMContentLoaded", init);
  * @description Adds click event to clicker page where needed
  */
 function clicker_buttons(){
-
-    const clicker_comp = document.getElementById("clicker_comp")
+    const clicker_comp = document.getElementById("clicker-comp");
+    const points_display = document.getElementById("points_display");
     if (!clicker_comp){
       return;
     }
-    clicker_comp.addEventListener("click", function(){
+    clicker_comp.addEventListener("mousedown", function(){
       /*
       TODO: Use update_points to update the # of points. So you dont call the function
       a ton of times, I recommend using setTimeout so
       like every 500 ms save points with update_points function.
       */
-      console.log("clicked")
+      // console.log("clicked")
+      update_points(1);
+      let user_info = JSON.parse(localStorage.getItem("user_data"));
+      if (!user_info){
+        console.error("No user info found");
+        return;
+      }
+      // else{
+      //   console.log(user_info.points);
+      // }
+      points_display.innerHTML = `Points: ${user_info.points}`;
+      clicker_comp.style.transform = "scale(0.95)";
     })
-  
-  
-  
-  }
+    clicker_comp.addEventListener("mouseup", function(){
+      /*
+      TODO: Use update_points to update the # of points. So you dont call the function
+      a ton of times, I recommend using setTimeout so
+      like every 500 ms save points with update_points function.
+      */      
+     
+      clicker_comp.style.transform = "scale(1)";
+    })
+    /**
+     * Updates the user point in the backend and the UI.
+     * Calls update_points() with the new points.
+     */
+    
+    // Update the user points
+    // call update_points with the new points
+    // update the UI with the new pointsg
 
-  function init(){
+
+}
+
+
+
+  async function init(){
     clicker_buttons()
+    const points_display = document.getElementById("points_display");
+    let user_info = JSON.parse(localStorage.getItem("user_data"));
+    points_display.innerHTML =`Points: ${user_info.points}`;
   }
